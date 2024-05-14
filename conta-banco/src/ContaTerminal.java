e6fdc358b5eca37eba43f65ea2981debb72881a0
dre36dre <dre.son36@gmail.com>
2024-05-14 05:49:38 -0300

    Meu projeto conta banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,5 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hoje estou muito feliz por ter passado na entrevista");
+    }
+}
