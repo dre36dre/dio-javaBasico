ffb3b6cbae34f0940ac03f53f7b79ca1a67c86d8
dre36dre <dre.son36@gmail.com>
2024-05-15 06:12:43 -0300

    consulta de saldo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hoje estou muito feliz por ter passado na entrevista");
-    }
+        Scanner entrada= new Scanner(System.in);
+        
+        int conta=0;
+        String agencia;
+        String nomeCliente;
+        double saldo=237.54;
+
+        System.out.println("Por favor digite o numero da conta");
+        conta=entrada.nextInt();
+        
+        System.out.println("Por favor digite a agência");
+        agencia=entrada.nextLine();
+
+        System.out.println("Digite o nome");
+        nomeCliente=entrada.nextLine();
+
+        System.out.println("Olá"  +nomeCliente+ 
+        ", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+
+        ", conta "+conta+ "e seu saldo"+saldo+ "esta disponível para saque");
+
+
+
+        entrada.close();
+}
 }
